facca5f6b7e394b91cb1982cf0615574d89e1c5e
kenndave <dave.bahana@gmail.com>
2023-02-24 11:01:15 +0700

    Masih build class

---
+++ src/visual.py
@@ -0,0 +1,20 @@
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+import numpy as np
+import 'main.py'
+# Creating dataset
+z = np.random.randint(100, size =(50))
+x = np.random.randint(80, size =(50))
+y = np.random.randint(60, size =(50))
+p = Point(x, y, z)
+ 
+# Creating figure
+fig = plt.figure(figsize = (10, 7))
+ax = plt.axes(projection ="3d")
+ 
+# Creating plot
+ax.scatter3D(p.x, p.y, p.z, color = "blue")
+plt.title("simple 3D scatter plot")
+ 
+# show plot
+plt.show()
