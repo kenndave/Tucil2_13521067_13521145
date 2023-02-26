1d3c73807db6989b0594527ac95a37a668bce991
kenndave <dave.bahana@gmail.com>
2023-02-26 16:26:08 +0700

    Update bonus 2

--- src/DivQonq.py
+++ src/DivQonq.py
@@ -6,12 +6,6 @@ import matplotlib.pyplot as plt
 
 euclid_count = 0
 
-class Point:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
 def quickSort(list, key):
     if len(list) <= 1:
         return list
@@ -20,34 +14,41 @@ def quickSort(list, key):
 
     greaterPivot = []
     lesserPivot = []
-    if (key == 0):
-        for titik in list:
-            if titik.x > pivot.x:
-                greaterPivot.append(titik)
-            else:
-                lesserPivot.append(titik)
-    elif (key == 1):
-        for titik in list:
-            if titik.y > pivot.y:
-                greaterPivot.append(titik)
-            else:
-                lesserPivot.append(titik)
-    elif (key == 2):
-        for titik in list:
-            if titik.z > pivot.z:
-                greaterPivot.append(titik)
-            else:
-                lesserPivot.append(titik)
+    for titik in list:
+        if titik[key] > pivot[key]:
+            greaterPivot.append(titik)
+        else:
+            lesserPivot.append(titik)
+    # if (key == 0):
+    #     for titik in list:
+    #         if titik.x > pivot.x:
+    #             greaterPivot.append(titik)
+    #         else:
+    #             lesserPivot.append(titik)
+    # elif (key == 1):
+    #     for titik in list:
+    #         if titik.y > pivot.y:
+    #             greaterPivot.append(titik)
+    #         else:
+    #             lesserPivot.append(titik)
+    # elif (key == 2):
+    #     for titik in list:
+    #         if titik.z > pivot.z:
+    #             greaterPivot.append(titik)
+    #         else:
+    #             lesserPivot.append(titik)
     
     return quickSort(lesserPivot, key) + [pivot] + quickSort(greaterPivot, key)
 
 def dist(p1, p2):
     global euclid_count
     euclid_count += 1
-    return math.sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2 + (p1.z - p2.z)**2)
+    disst = 0
+    for i in range(len(p1)):
+        disst += (p1[i] - p2[i])**2
+    return math.sqrt(disst)
 
 def bruteForce(P, n):
-    i = 0
     min_dist = float("inf")
     for i in range(n):
         for j in range(i+1, n):
@@ -57,15 +58,24 @@ def bruteForce(P, n):
     return closest_pair
 
 def min(x, y):
-    return x if x < y else y
+    if x < y:
+        return x
+    else:
+        return y
 
 def stripClosest(strip, size, d, closest_pair):
     min_dist = d
-    strip = quickSort(strip, 2)
+    if len(strip[0]) < 2:
+        #print(len(strip))
+        sumbu = 0
+        strip = quickSort(strip, sumbu)
+    else:
+        sumbu = 1
+        strip = quickSort(strip, sumbu)
     #printPoints(strip)
     for i in range(size):
         for j in range(i+1, size):
-            if (strip[j].z - strip[i].z) >= min_dist:
+            if (strip[j][sumbu] - strip[i][sumbu]) >= min_dist:
                 break
             if dist(strip[i], strip[j]) < min_dist:
                 min_dist = dist(strip[i], strip[j])
@@ -82,57 +92,108 @@ def closestUtil(P, n):
     dl = dist(*dl_pair)
     dr = dist(*dr_pair)
     d = min(dl, dr)
-    closest_pair = dl_pair if dl < dr else dr_pair
+    if dl < dr:
+        closest_pair = dl_pair
+    else:
+        closest_pair = dr_pair
+    #closest_pair = dl_pair if dl < dr else dr_pair
     strip = []
     for i in range(n):
-        if abs(P[i].x - midPoint.x) < d:
+        if abs(P[i][0] - midPoint[0]) < d:
             strip.append(P[i])
-    closest_pair_in_strip = stripClosest(strip, len(strip), d, closest_pair)
-    closest_pair = closest_pair if dist(*closest_pair) < dist(*closest_pair_in_strip) else closest_pair_in_strip
-    return closest_pair
+    if len(strip) != 0:
+        closest_pair_in_strip = stripClosest(strip, len(strip), d, closest_pair)
+        if dist(*closest_pair) < dist(*closest_pair_in_strip):
+            return closest_pair
+        else:
+            return closest_pair_in_strip
+        
+    else:
+        return closest_pair
 
 def closest(P, n):
     P = quickSort(P, 0)
     return closestUtil(P, n)
 
-def createRandomPoints(n):
+def createRandomPoints(n, dimension):
     points = []
     for i in range(n):
-        x = round(random.uniform(1, 100), 2)
-        y = round(random.uniform(1, 100), 2)
-        z = round(random.uniform(1, 100), 2)
-        p = Point(x, y, z)
+        p = []
+        for j in range(dimension):
+            p.append(round(random.uniform(1, 100), 2))
         points.append(p)
+    #print(points)
     return points
 
 def printPoints(points):
     print("The randomize points are,")
     for point in points:
-        print(f"({point.x}, {point.y}, {point.z})")
+        print("(", end='')
+        for i in range(len(points[0])):
+            print(point[i], end= '')
+            if i < len(points[0])-1:
+                print(", ")
+        print(")")
 
 def printPointPair(pair):
     p1, p2 = pair
-    print("Point 1: ({0}, {1}, {2})".format(p1.x, p1.y, p1.z))
-    print("Point 2: ({0}, {1}, {2})".format(p2.x, p2.y, p2.z))
+    print("Point 1: (", end='')
+    for j in range(len(p1)):
+        print (p1[j], end= '')
+        if (j < len(p1)-1):
+            print (', ', end ='')
+    print(")")
+
+    print("Point 2: (", end= '')
+    for j in range(len(p2)):
+        print (p2[j], end='')
+        if (j < len(p2)-1):
+            print (', ', end= '')
+    print(")")
+
 
 def visualizePoints(points, closest_pair=None):
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
- 
-    x = [point.x for point in points]
-    y = [point.y for point in points]
-    z = [point.z for point in points]
- 
+    if len(points[0]) == 3:
+        ax = fig.add_subplot(111, projection='3d')
+    elif len(points[0]) == 2:
+        ax = fig.add_subplot(111)
+    else:
+        ax = fig.add_subplot(111)
+    x = []
+    for i in range(len(points[0])):
+        x.append([point[i] for point in points])
     if closest_pair:
         p1, p2 = closest_pair
- 
-        ax.scatter(p1.x, p1.y, p1.z, color='r', s=100)
-        ax.scatter(p2.x, p2.y, p2.z, color='r', s=100)
-    ax.plot([p1.x, p2.x], [p1.y, p2.y], [p1.z, p2.z], color = 'r', linewidth = 5)
-    ax.scatter(x, y, z)
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
+        if (len(p1) == 3):
+            ax.scatter(p1[0], p1[1], p1[2], color='r', s=100)
+            ax.scatter(p2[0], p2[1], p2[2], color='r', s=100)
+        elif (len(p1) == 2):
+            ax.scatter(p1[0], p1[1], color='r', s=100)
+            ax.scatter(p2[0], p2[1], color='r', s=100)
+        else:
+            ax.plot(p1[0], 0.0, color='r')
+            ax.plot(p2[0], 0.0, color='r')
+
+    if (len(p1) == 3):
+        ax.plot([p1[0], p2[0]], [p1[1], p2[1]], [p1[2], p2[2]], color = 'r', linewidth = 5)
+        ax.scatter(x[0], x[1], x[2])
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+
+    elif (len(p1) == 2):
+        ax.plot([p1[0], p2[0]], [p1[1], p2[1]], color = 'r', linewidth = 5)
+        ax.scatter(x[0], x[1])
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+
+    else:
+        y = [0 for i in range(len(x[0]))]
+        ax.plot([p1[0], p2[0]], [0, 0], color = 'r', linewidth = 5)
+        ax.scatter(x[0], y)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
  
     plt.show()
 
@@ -144,35 +205,62 @@ if __name__ == "__main__":
     print()
 
     number_of_points = int(input("Input the number of points: "))
-    P = createRandomPoints(number_of_points)
+    dimens = int(input("Dimensi: "))
+    while dimens <= 0:
+        print("Dimensi tersebut tidak valid. Silahkan input ulang dimensi dengan nilai 1 atau ke atas.")
+        dimens = int(input("Dimensi: "))
+    P = []
+    P = createRandomPoints(number_of_points, dimens)
     P2 = P.copy()
-    printPoints(P)
+    # printPoints(P)
     print()
 
-    
-    # Divide & Conquer
-    start_time = time.time()
-    closest_pair = closest(P, number_of_points)
-    end_time = time.time()
-    printPoints(P2)
+
     # BruteForce
     start_bf = time.time()
     closest_pair2 = bruteForce(P2, number_of_points)
     end_bf = time.time()
+    # Divide & Conquer
+    euclid_count_bf = euclid_count
+
+    start_time = time.time()
+    closest_pair = closest(P, number_of_points)
+    end_time = time.time()
+    # printPoints(P2)
 
     
     print()
     print("The closest pair of points are, (Divide & Conquer)")
     printPointPair(closest_pair)
+    print()
     print("The closest pair of points are, (bruteForce)")
     printPointPair(closest_pair2)
     print()
 
     print("The distance is for Divide & Conquer", dist(*closest_pair)) #round(dist(*closest_pair), 2))
-    print("The distance for Brute Force is", dist(*closest_pair2))
-    print("The number of Euclidian formula operation is", euclid_count)
-    print("Time taken by Divide & Conquer ", end_time - start_time, "seconds")
-    print("Time taken by bruteForce ", end_bf - start_bf, " seconds")
     print()
-
-    visualizePoints(P, closest_pair)
+    print("The distance for Brute Force is", dist(*closest_pair2))
+    print()
+    print("The number of Euclidian formula operation (Divide & Conquer) is", euclid_count-euclid_count_bf)
+    print("The number of Euclidian formula operation (Brute Force) is", euclid_count_bf)
+    print("Time taken by Divide & Conquer ", 1000*(end_time - start_time), "miliseconds")
+    print("Time taken by Brute Force ", 1000*(end_bf - start_bf), " miliseconds")
+    print()
+    if dimens <= 3 & dimens > 0:
+            visualize = input("Apakah ingin ditampilkan visualisasinya? (Y / N)")
+            while visualize not in {'Y', 'N', 'y', 'n'}:
+                print("Input tidak valid. Silahkan input ulang!")
+                visualize = input("Apakah ingin ditampilkan visualisasinya?(Y/N) ")
+            if (visualize in {'Y', 'y'}):
+                visualizePoints(P, closest_pair)
+    else:
+        print("Maaf, tidak dapat divisualisasikan pada dimensi,", dimens)
+    # if dimens <= 3 & dimens > 0:
+    #     visualize = (input("Apakah ingin ditampilkan visualisasinya?(Y/N) "))
+    #     while not (visualize == 'Y' or visualize == 'y' or visualize == 'N' or visualize == 'n'):
+    #         print(visualize)
+    #         print("Input tidak valid. Silahkan input ulang!")
+    #         visualize = input("Apakah ingin ditampilkan visualisasinya?(Y/N) ")
+    #     visualizePoints(P, closest_pair)
+    # else:
+    #     print("Maaf tidak dapat divisualisasikan untuk dimensi ", dimens, ".")
